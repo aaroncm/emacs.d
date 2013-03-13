df7b87de1d75fcd928080ba14ddc7f4c26f66ab8
Aaron Malone <aaron@munge.net>
2013-03-13 12:05:54 -0500

    go and sml

--- lisp/aaron-packages.el
+++ lisp/aaron-packages.el
@@ -4,7 +4,8 @@
 (defvar aaron-packages
   '(linum clojure-mode color-theme-sanityinc-tomorrow evil evil-leader
           evil-numbers auto-indent-mode haskell-mode sml-mode
-          highlight-parentheses key-chord magit pep8 pylint slime)
+          highlight-parentheses key-chord magit pep8 pylint slime
+          go-mode sml-mode)
   "packages to install at launch")
 
 ;; we need dash for the loop constructs
