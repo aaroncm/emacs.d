16390f1902db7b6f8b99558c2a0994ac70325c3a
Aaron Malone <aaron@gammahealthcare.com>
2013-03-18 09:46:38 -0500

    remove redundant mode, fix tab

--- init.el
+++ init.el
@@ -3,7 +3,7 @@
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives
-	     '("melpa" . "http://melpa.milkbox.net/packages/"))
+             '("melpa" . "http://melpa.milkbox.net/packages/"))
 (require 'aaron-packages)
 
 (setq inhibit-startup-message t)
