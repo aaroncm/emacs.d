42db57cbb997f77e80cb7ea177ed6486264e240c
Aaron Malone <aaron@munge.net>
2013-05-05 21:02:04 -0500

    linum workaround no longer needed

--- init.el
+++ init.el
@@ -104,22 +104,6 @@
 ;; line numbers
 (setq column-number-mode t)
 (global-linum-mode 1)
-;; rendering workaround
-(eval-after-load 'linum
-  '(progn
-     (defface linum-leading-zero
-       `((t :inherit 'linum
-            :foreground ,(face-attribute 'linum :background nil t)))
-       "Face for displaying leading zeroes for line numbers in display margin."
-       :group 'linum)
-     (defun linum-format-func (line)
-       (let ((w (length
-                 (number-to-string (count-lines (point-min) (point-max))))))
-         (concat
-          (propertize (make-string (- w (length (number-to-string line))) ?0)
-                      'face 'linum-leading-zero)
-          (propertize (number-to-string line) 'face 'linum))))
-     (setq linum-format 'linum-format-func)))
 
 ;; flycheck
 (add-hook 'prog-mode-hook 'flycheck-mode)
